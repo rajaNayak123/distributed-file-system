d19a379e3bc01cddec0b562a4c9363a07ba1f2ed
rajaNayak123 <nayakraja151@gmail.com>
2026-08-17 22:34:46 +0530

    add file creation repository with DynamoDB conflict handling

---
+++ apps/api/src/repositories/files.repository.js
@@ -0,0 +1,39 @@
+import MetadataService from "../services/metadata.service.js"
+import config from '../config/index.js';
+import { ConflictError } from '../utils/errors.js';
+
+export default class FilesRepository{
+  constructor(metadataService = new MetadataService(), tableName = config.dynamo.filesTable){
+    this.metadataService = metadataService
+    this.tableName = tableName
+  }
+
+  static pk(userId) {
+    return `USER#${userId}`;
+  }
+
+  static sk(fileId) {
+    return `FILE#${fileId}`;
+  }
+
+  async createFile(fileItem) {
+    const item = {
+      PK: FilesRepository.pk(fileItem.userId),
+      SK: FilesRepository.sk(fileItem.fileId),
+      ...fileItem,
+    };
+    try {
+      await this.metadataService.putItem({
+        tableName: this.tableName,
+        item,
+        conditionExpression: 'attribute_not_exists(PK)',
+      });
+      return item;
+    } catch (err) {
+      if (err.name === 'ConditionalCheckFailedException') {
+        throw new ConflictError('A file with this id already exists');
+      }
+      throw err;
+    }
+  }
+}
