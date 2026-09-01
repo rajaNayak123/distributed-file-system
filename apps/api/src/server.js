e58eb97f9790fd322fd6f95456729a4d2590e2be
rajaNayak123 <nayakraja151@gmail.com>
2026-09-01 22:21:10 +0530

    wire multipart upload cleanup cron job on startup

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -2,8 +2,14 @@ import createApp from './app.js';
 import config from './config/index.js';
 import logger from './utils/logger.js';
 
+import { startCleanupJob } from './cron/cleanup.js';
+
 const app = createApp();
 
 app.listen(config.port, () => {
   logger.info('server_started', { operation: 'startup', status: 'OK', port: config.port, env: config.env });
+  
+  if (config.env !== 'test') {
+    startCleanupJob();
+  }
 });
