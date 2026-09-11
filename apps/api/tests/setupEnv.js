769dbf3627b3d0c1cbf422ae70cc2aef2a66a9f0
rajaNayak123 <nayakraja151@gmail.com>
2026-09-11 11:26:20 +0530

    test(api): add test environment variable setup

---
+++ apps/api/tests/setupEnv.js
@@ -0,0 +1,10 @@
+process.env.NODE_ENV = 'test';
+process.env.JWT_ACCESS_SECRET = 'test-access-secret';
+process.env.JWT_REFRESH_SECRET = 'test-refresh-secret';
+process.env.JWT_ACCESS_EXPIRES_IN = '15m';
+process.env.JWT_REFRESH_EXPIRES_IN = '7d';
+process.env.S3_BUCKET = 'test-bucket';
+process.env.DYNAMODB_FILES_TABLE = 'Files-test';
+process.env.DYNAMODB_USERS_TABLE = 'Users-test';
+process.env.PRESIGNED_PUT_EXPIRY_SECONDS = '900';
+process.env.PRESIGNED_GET_EXPIRY_SECONDS = '300';
