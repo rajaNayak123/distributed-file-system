97b1e55eef67e3ed35674e347e60bd02029f50c6
rajaNayak123 <nayakraja151@gmail.com>
2026-09-12 13:42:23 +0530

    refactor(api): clean schema comments in users repository

--- apps/api/src/repositories/users.repository.js
+++ apps/api/src/repositories/users.repository.js
@@ -2,12 +2,6 @@ import MetadataService from "../services/metadata.service.js"
 import config from '../config/index.js';
 import { ConflictError } from '../utils/errors.js';
 
-/*
- Users table key schema:
-    PK: USER#<userId> "Partition Key", The PK is used to identify which partition/group an item belongs to.
-    SK: PROFILE "Sort Key", The SK gives another level of identification inside the same PK.
- */
-
 export default class UsersRepository{
     constructor(metadataService = new MetadataService(), tableName = config.dynamo.usersTable){
         this.metadataService = metadataService
